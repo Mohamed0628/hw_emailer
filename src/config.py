fc1a12a5d007f4042a095b51730b6fb3c9371557
Mohamed0628 <ahme0712@umn.edu>
2026-07-27 00:40:10 -0500

    Load batch 10 company sites

--- src/config.py
+++ src/config.py
@@ -88,6 +88,7 @@ def companies() -> dict[str, Any]:
         "companies_batch_07.yaml",
         "companies_batch_08.yaml",
         "companies_batch_09.yaml",
+        "companies_batch_10.yaml",
     )
 
 
