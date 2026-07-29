601d42d4f2ecd93637eefacb2a956478cb45ae98
Mohamed0628 <ahme0712@umn.edu>
2026-07-29 15:57:39 -0500

    chore: load verified company batch 19

--- src/config.py
+++ src/config.py
@@ -97,6 +97,7 @@ def companies() -> dict[str, Any]:
         "companies_batch_16.yaml",
         "companies_batch_17.yaml",
         "companies_batch_18.yaml",
+        "companies_batch_19.yaml",
     )
 
 
