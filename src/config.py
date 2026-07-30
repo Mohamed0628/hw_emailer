2bae37640416e985c33c0f0eb514d2dc2dc3a401
Mohamed0628 <ahme0712@umn.edu>
2026-07-29 19:58:57 -0500

    feat: load company source batch 22

--- src/config.py
+++ src/config.py
@@ -100,6 +100,7 @@ def companies() -> dict[str, Any]:
         "companies_batch_19.yaml",
         "companies_batch_20.yaml",
         "companies_batch_21.yaml",
+        "companies_batch_22.yaml",
     )
 
 
