666e5e95cdff463cfa1f7ccfc2265c096bf1a705
Mohamed0628 <ahme0712@umn.edu>
2026-07-30 04:58:53 -0500

    Load companies batch 31

--- src/config.py
+++ src/config.py
@@ -109,6 +109,7 @@ def companies() -> dict[str, Any]:
         "companies_batch_28.yaml",
         "companies_batch_29.yaml",
         "companies_batch_30.yaml",
+        "companies_batch_31.yaml",
     )
 
 
